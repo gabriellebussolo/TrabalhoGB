0bf63f2546f5dcc95403b29e472c60834570e72d
gabriellebussolo <gabrielle.bussolo@gmail.com>
2024-06-19 19:37:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,19 +15,18 @@ print('3 - Binarização')
 
 opcao = input()
 
-while(opcao != '0'):
-    if opcao == '1':
-        ft.grayscale(img2)
-        cv.imshow('Grayscale', img2)
-        k = cv.waitKey(0) # faz com que se eu clicar em qualquer tecla a imagem feche
-    elif opcao == '2':
-        ft.negativo(img2)
-        cv.imshow('Negativo', img2)
-        k = cv.waitKey(0) # faz com que se eu clicar em qualquer tecla a imagem feche
-    elif opcao == '3':
-        print('Insira o linear que deverá ser considerado:')
-        l = int(input())
-        ft.binarizacao(img2, l)
-        cv.imshow('binarizacao', img2)
-        k = cv.waitKey(0) # faz com que se eu clicar em qualquer tecla a imagem feche
-    opcao = input()
+if opcao == '1':
+    ft.grayscale(img2)
+    cv.imshow('Grayscale', img2)
+    k = cv.waitKey(0) # faz com que se eu clicar em qualquer tecla a imagem feche
+elif opcao == '2':
+    ft.negativo(img2)
+    cv.imshow('Negativo', img2)
+    k = cv.waitKey(0) # faz com que se eu clicar em qualquer tecla a imagem feche
+elif opcao == '3':
+    print('Insira o linear que deverá ser considerado:')
+    l = int(input())
+    ft.binarizacao(img2, l)
+    cv.imshow('binarizacao', img2)
+    k = cv.waitKey(0) # faz com que se eu clicar em qualquer tecla a imagem feche
+opcao = input()
