dac36d642e2dd38fe8b61e373ed94ed8203d010c
gabriellebussolo <gabrielle.bussolo@gmail.com>
2024-06-19 19:34:39 -0300

    adicionando binarizacao

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ img2 = img.copy() # cria uma copia da original
 
 print('Escolha uma opcao de filtro:\n')
 print('0 - Sair')
-print('1 - Grayscale\n')
+print('1 - Grayscale')
 print('2 - Negativo')
+print('3 - Binarização')
 
 opcao = input()
 
@@ -22,5 +23,11 @@ while(opcao != '0'):
     elif opcao == '2':
         ft.negativo(img2)
         cv.imshow('Negativo', img2)
+        k = cv.waitKey(0) # faz com que se eu clicar em qualquer tecla a imagem feche
+    elif opcao == '3':
+        print('Insira o linear que deverá ser considerado:')
+        l = int(input())
+        ft.binarizacao(img2, l)
+        cv.imshow('binarizacao', img2)
         k = cv.waitKey(0) # faz com que se eu clicar em qualquer tecla a imagem feche
     opcao = input()
